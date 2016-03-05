77f1b626f6b09dbe18bbf028cf28914ff52da44a
Derek Fludzinski <thesource@Dereks-MacBook-Air.local>
2016-03-05 15:10:44 -0500

    updating basic server html app and npm

--- server.js
+++ server.js
@@ -1,2 +1,36 @@
+////////////////////
+// REQUIREMENTS
+////////////////////
+
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var port = process.env.PORT || 3000;
+
+////////////////////
+// MIDDLEWARE
+////////////////////
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+////////////////////
+// CONTROLLERS
+////////////////////
+
+////////////////////
+// LISTEN
+////////////////////
+
+// mongoose.connect('mongodb://localhost:27017/urbanyoda');
+
+db.once('open', function(){
+  app.listen(port, function(){
+    console.log('====================');
+    console.log('Running on Port: ' + port);
+    console.log('====================');
+  });
+});
