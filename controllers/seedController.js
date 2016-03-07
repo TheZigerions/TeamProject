fc2c8a75d414859fd51c97f042bb4d9875f3828c
Derek Fludzinski <thesource@Dereks-MacBook-Air.local>
2016-03-07 10:25:08 -0500

    fixed formatting in seed route

--- controllers/seedController.js
+++ controllers/seedController.js
@@ -12,16 +12,16 @@ var newCharacter = [
   			image: 'http://vignette2.wikia.nocookie.net/disney/images/9/95/Master_Yoda.png/revision/latest?cb=20151112212224'
 		}, {
 			name: 'Chewbacca',
-    	    img: 'http://vignette3.wikia.nocookie.net/disney/images/d/da/TFA-Chewbacca-Fathead.png/revision/latest?cb=20160207022126'
+    	    image: 'http://vignette3.wikia.nocookie.net/disney/images/d/da/TFA-Chewbacca-Fathead.png/revision/latest?cb=20160207022126'
 	  	}, {
 			name: 'Leia',
-    	    img: 'http://vignette4.wikia.nocookie.net/disney/images/7/7f/Princess_Leia_render.png/revision/latest?cb=20151220170346'
+    	    image: 'http://vignette4.wikia.nocookie.net/disney/images/7/7f/Princess_Leia_render.png/revision/latest?cb=20151220170346'
 	  	}, {
 			name: 'Darth Vader',
-    	    img: 'http://vignette3.wikia.nocookie.net/disney/images/d/df/Darth_Vader_Render.png/revision/latest?cb=20151222170300'
+    	    image: 'http://vignette3.wikia.nocookie.net/disney/images/d/df/Darth_Vader_Render.png/revision/latest?cb=20151222170300'
 	  	}, {
 			name: 'R2D2',
-	        img:'http://vignette3.wikia.nocookie.net/disney/images/7/7c/R2-D2_Render.png/revision/latest?cb=20130314222826'
+	        image:'http://vignette3.wikia.nocookie.net/disney/images/7/7c/R2-D2_Render.png/revision/latest?cb=20130314222826'
 		}
 ];
 
